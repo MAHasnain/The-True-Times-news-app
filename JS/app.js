3d01ba0de6b03186eac74b621d7c5ba7a54f4f73
Has4276* <attaulhasnainmaqsoodi@gmail.com>
2025-08-29 09:32:41 +0500

    news data fetching functionality

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,8 @@
 const API_KEY = "75cf45673abd4cd8b24e93aba1200290";
 
 const query = "bitcoin"
-const standardEndpoint = "https://newsapi.org/v2"
-    
+const standardEndpoint = "https://newsapi.org/v2";
+
 `${standardEndpoint}/everything?q=${query}&apiKey=${API_KEY}`;
 `${standardEndpoint}/everything?q=${query}&from=2025-08-26&to=2025-08-26&sortBy=popularity&apiKey=${API_KEY}`;
 `${standardEndpoint}/everything?domains=techcrunch.com,thenextweb.com&apiKey=${API_KEY}`;
@@ -11,3 +11,21 @@ const standardEndpoint = "https://newsapi.org/v2"
 `${standardEndpoint}/top-headlines?country=pk&category=business&apiKey=${API_KEY}`;  /// Countries  ae ar at au be bg br ca ch cn co cu cz de eg fr gb gr hk hu id ie il in it jp kr lt lv ma mx my ng nl no nz ph pl pt ro rs ru sa se sg si sk th tr tw ua us ve za
 `${standardEndpoint}/top-headlines?q=trump&apiKey=${API_KEY}`;
 `${standardEndpoint}/top-headlines/sources?category=businessapiKey=${API_KEY}`; /// CATEGORIES => business  entertainment  general  health  science  sports  technology
+
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+
+    try {
+        const res = await fetch(`${standardEndpoint}/top-headlines?country=us&apiKey=${API_KEY}`)
+        const data = await res.json()
+
+        console.log(data);
+
+        console.log(data.articles[5].content);
+
+    } catch (error) {
+        console.error(error);
+    }
+
+})
