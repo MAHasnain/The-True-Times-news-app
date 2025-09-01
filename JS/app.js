702eb3c590cf5e771d7305d2eba19c0f3e0dc743
Muhammad_Atta_Ul_Hasnain <attaulhasnainmaqsoodi@gmail.com>
2025-09-01 08:39:04 +0500

    Add layout structure for news cards and side section in HTML and CSS

--- JS/app.js
+++ JS/app.js
@@ -8,7 +8,7 @@ const standardEndpoint = "https://newsapi.org/v2";
 `${standardEndpoint}/everything?domains=techcrunch.com,thenextweb.com&apiKey=${API_KEY}`;
 `${standardEndpoint}/top-headlines?country=us&apiKey=${API_KEY}`;
 `${standardEndpoint}/top-headlines?sources=bbc-news&apiKey=${API_KEY}`;
-`${standardEndpoint}/top-headlines?country=pk&category=business&apiKey=${API_KEY}`;  /// Countries  ae ar at au be bg br ca ch cn co cu cz de eg fr gb gr hk hu id ie il in it jp kr lt lv ma mx my ng nl no nz ph pl pt ro rs ru sa se sg si sk th tr tw ua us ve za
+`${standardEndpoint}/top-headlines?country=pk&category=business&apiKey=${API_KEY}`;
 `${standardEndpoint}/top-headlines?q=trump&apiKey=${API_KEY}`;
 `${standardEndpoint}/top-headlines/sources?category=businessapiKey=${API_KEY}`; /// CATEGORIES => business  entertainment  general  health  science  sports  technology
 
@@ -25,14 +25,23 @@ function timeUpdate() {
     setTimeout(timeUpdate, 1000);
 }
 timeUpdate()
-console.log(date);
 
+const news_card = document.querySelector(".news-card")
 document.addEventListener("DOMContentLoaded", async () => {
     try {
-        const res = await fetch(`${standardEndpoint}/top-headlines?country=us&apiKey=${API_KEY}`)
+        // const res = await fetch(`${standardEndpoint}/top-headlines?country=pk&apiKey=${API_KEY}`)
+        const res = await fetch(`${standardEndpoint}/top-headlines?country=us&category=science&apiKey=${API_KEY}`)
         const data = await res.json()
 
-        console.log(data);
+        // console.log(data.articles);
+        data.articles.map(article => {
+            console.log(article);
+            news_card.innerHTML = `
+           <div class="txt-content">
+                        <h3 class="title">${article.title}</h3>
+                        <p class="text">${article.content}</p>
+                    </div>
+                    <div class="img"></div>`})
 
     } catch (error) {
         console.error(error);
